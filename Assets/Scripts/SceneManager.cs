70a0ab5b6239e315372fb8da83c4c45166810474
John Peterson <johnp5419@gmail.com>
2022-10-29 09:31:47 -0600

    Added Cube instantiation with a prefab 6 color cubelet

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class SceneManager : MonoBehaviour
 {
-    public GameObject cube;
+    public GameObject cubelet;
+    public int cubeDim = 3;
 
     public void spawnCube()
     {
-        Instantiate(cube);
+        for (int i = 0; i < cubeDim; i++)
+        {
+            for (int j = 0; j < cubeDim; j++)
+            {
+                for (int k = 0; k < cubeDim; k++)
+                {
+                    float x = (i - Mathf.Floor(cubeDim / 2)) / cubeDim * 2;
+                    float y = (j - Mathf.Floor(cubeDim / 2)) / cubeDim * 2;
+                    float z = (k - Mathf.Floor(cubeDim / 2)) / cubeDim * 2;
+
+                    GameObject cubeletGO = Instantiate(cubelet);
+                    cubeletGO.transform.localScale -= new Vector3(0.666f, 0.666f, 0.666f);
+                    cubeletGO.transform.position = new Vector3(x, y, z);
+                }
+            }
+        }
     }
 
     public void Quit() 

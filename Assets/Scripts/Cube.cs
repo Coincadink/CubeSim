fd9771900998dad41ed551f524c2a1fed0b0f356
Tim Lawrence <timothyjl2002@gmail.com>
2022-11-07 11:25:49 -0700

    Implement turns for 1D slices

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using HollowArray;
 
 /// <summary>
-/// Represents a Rubix cube, as a collection of rotated cubelets.
-/// Contains six faces, labeled 1-6. Each cubelet is named by its face, then its position on the face, ordered left to right, top to bottom.
-/// For example, the cubelet at [1, 12] on a 5x5 cube is directly to the left of the center cubelet on the 1st face.
+/// Represents a Rubix cube as a collection of rotated cubelets.
+/// Contains six faces, labeled 1-6. Faces are labeled in order Y=0, Y=size, Z=0, Z=size, X=0, X=size.
+/// Each cubelet can also be named by its position in 3D space, where [0,0,0] is the bottom right front corner.
 /// </summary>
 public class Cube {
     private HollowArray<byte> cubelets;
     private readonly int size;
+    readonly LinkedList<Slice> moveHistory;
 
     /// <summary>
     /// Creates a new cube.
@@ -19,6 +20,7 @@ public class Cube {
     {
         this.size = size;
         cubelets = new(size);
+        moveHistory = new();
     }
 
     /// <summary>
@@ -47,23 +49,46 @@ public class Cube {
     public byte this[int x, int y, int z] => GetCubelet(x, y, z);
 
     /// <summary>
-    /// Rotate one of the cube's slices, where a positive <paramref name="face"/> indicates a clockwise turn, while a negative indicates a counter-clockwise turn.
+    /// Rotate one of the cube's slices, where <paramref name="dir"/> = true indicateds a clockwise turn, while false indicates a counter-clockwise turn.
     /// A slice is defined as a square plane of cubelets, named relative to its nearest parallel <paramref name="face"/>. For example, on a 4x4 cube, the slice directly beneath <paramref name="face"/> 0 is <paramref name="depth" /> 1.
     /// </summary>
-    /// <param name="face">The nearest parallel face of the slice.</param>
-    /// <param name="depth">Distance of the slice from the named <paramref name="face"/>.</param>
-    public void Turn(int face, int depth = 0)
+    /// <param name="axis">The axis perpendicular to the slice. 0=Y, 1=Z, 2=X.</param>
+    /// <param name="dir">The direction to turn the slice in.</param>
+    /// <param name="depth">Coordinate position of the slice along the named <paramref name="axis"/>.</param>
+    public void Turn(int axis, bool dir, int depth = 0)
     {
-        Turn(new Slice { Face = face, Depth = depth });
+        Turn(new Slice { Axis = axis, Dir = dir, Depth = depth });
     }
 
     /// <summary>
-    /// Rotate one of the cube's slices, where a positive face indicates a clockwise turn, while a negative indicates a counter-clockwise turn.
+    /// Rotate one of the cube's slices, where <see cref="Slice.Dir"/> = true indicateds a clockwise turn, while false indicates a counter-clockwise turn.
     /// </summary>
     /// <param name="turn">The slice to turn.</param>
     public void Turn(Slice turn)
     {
-        throw new NotImplementedException(); 
+        moveHistory.AddLast(turn);
+
+        Array slice = cubelets[turn.Depth, turn.Axis];
+        if (slice is byte[] sl1) // 1D slice in clockwise order
+        {
+            byte[] turnedSlice = new byte[sl1.Length];
+            if (turn.Dir) // clockwise
+            {
+                sl1[0..(sl1.Length - (size - 1))].CopyTo(turnedSlice, size - 1);
+                sl1[(sl1.Length - (size - 1))..sl1.Length].CopyTo(turnedSlice, 0);
+            }
+            else // counterclockwise
+            {
+                sl1[(size - 1)..sl1.Length].CopyTo(turnedSlice, 0);
+                sl1[0..(size - 1)].CopyTo(turnedSlice, sl1.Length - (size - 1));                
+            }
+
+            cubelets[turn.Depth, turn.Axis] = turnedSlice;
+        }
+        else if (slice is byte[,] sl2) // 2D slice in row by row order
+        {
+            throw new NotImplementedException();
+        }
     }
 
     /// <summary>
@@ -87,11 +112,12 @@ public class Cube {
 }
 
 /// <summary>
-/// Represents a square plane of cubelets, named relative to its nearest parallel face.
-/// For example, on a 4x4 cube, the slice directly beneath <see cref="Face"/> 3 is <see cref="Depth"/> 1.
+/// Represents a square plane of cubelets, named relative to its perpendicular axis.
+/// For example, on a 4x4 cube, face 4 is <see cref="Axis"/> 1, <see cref="Depth"/> 3.
 /// </summary>
 public struct Slice
 {
-    public int Face;
+    public int Axis;
     public int Depth;
+    public bool Dir;
 }
